1cb4668c400454cfb092968c61b2b77c06183ca0
developerfunnel@gmail.com <ahanda205@gmail.com>
2022-04-01 21:18:51 +0530

    code

--- apiWithUi.js
+++ apiWithUi.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 9700;
+const port = process.env.PORT || 9701;
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const bodyParser = require('body-parser');
